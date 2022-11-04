472dd20e6579c45d796920f49ee4a7c834f0da0f
Igboanugwo Collins <munisco12@gmail.com>
2022-11-04 05:37:38 +0100

    working on task 2

--- pages/api/task1.ts
+++ pages/api/task1.ts
@@ -11,9 +11,6 @@ export default function handler(
     req: NextApiRequest,
     res: NextApiResponse<Response>
 ) {
-
-    console.log(req.body)
-
     res.status(200).json({
         slackUsername: 'Niscolinx',
         backend: true,
